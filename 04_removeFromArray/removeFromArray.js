6624fb8b5849e1f619bc4f1969aa6e6dffd0da33
Tyler Moore <tmoore5252@yahoo.com>
2022-08-22 22:03:24 -0400

    First test passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function (arr, splice) {
+    console.log(arr);
+    let removed = arr.splice(splice-1, 1);
+    console.log(arr);
+    return arr;
+  };
+  
 // Do not edit below this line
 module.exports = removeFromArray;
