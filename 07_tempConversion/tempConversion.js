927a77a4ff8d0bedfc4ccc1d6af03e37719b203e
Tyler Moore <tylermoore5252@gmail.com>
2022-09-06 14:47:16 -0400

    Finished temp tempConversion tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  let celcius = (temp - 32) * (5 / 9);
+  return Math.round(celcius * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  let farenheit = temp * (9/5) + 32;
+  return Math.round(farenheit * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
