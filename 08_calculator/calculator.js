1804c6633b97f81be50109d1dd8e57e6dbd948b9
Tyler Moore <tylermoore5252@gmail.com>
2022-10-28 21:36:55 -0400

    Finished all Calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,43 @@
-const add = function() {
-	
+const add = function (a, b) {
+  let total = a + b;
+  return total;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  let total = a - b;
+  return total;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let total = 0;
+  for (let item of array) {
+    total += item;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let total = 1;
+  for (let item of array) {
+    total *= item;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function (number, power) {
+  return (total = number ** power);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) {
+    return n = 1;
+  } else {
+    for (i = n - 1; i >= 1; i--){
+     n *= i;
+    }
+    return n;
+  }
 };
-
 // Do not edit below this line
 module.exports = {
   add,
@@ -29,5 +45,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
