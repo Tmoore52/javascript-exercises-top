2057e1d0f670625a6358dea9e4159cd64b9d9fe2
Tyler Moore <tylermoore5252@gmail.com>
2022-10-29 08:43:21 -0400

    Edited RegEx expression

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let cleanString = string.toLowerCase().replace(/[^a-z]+/gi, "");
+  if (cleanString.split("").reverse().join("") === cleanString) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
